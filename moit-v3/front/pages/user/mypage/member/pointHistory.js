2da1814909dbf7ee5c6c05b4e8c030c606c800c4
look-93 <qhfk0961@gmail.com>
2026-09-09 23:22:24 +0900

    ci/cd(20)

--- moit-v3/front/pages/user/mypage/member/pointHistory.js
+++ moit-v3/front/pages/user/mypage/member/pointHistory.js
@@ -14,16 +14,16 @@ import {
   Statistic,
 } from 'antd';
 
-import {
-  BarChart,
-  Bar,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
-  ResponsiveContainer,
-} from 'recharts';
+// import {
+//   BarChart,
+//   Bar,
+//   XAxis,
+//   YAxis,
+//   CartesianGrid,
+//   Tooltip,
+//   Legend,
+//   ResponsiveContainer,
+// } from 'recharts';
 
 import {
   getPointHistoryRequest,
@@ -465,7 +465,7 @@ function pointHistory() {
                   width="100%"
                   height="100%"
                 >
-                  <BarChart
+                  {/* <BarChart
                     data={chartData}
                     margin={{
                       top: 20,
@@ -505,7 +505,7 @@ function pointHistory() {
                       fill="#ff4d4f"
                     />
 
-                  </BarChart>
+                  </BarChart> */}
                 </ResponsiveContainer>
               </div>
             </Card>
